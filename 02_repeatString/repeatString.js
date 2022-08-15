f7f13951d5b1a7320573785e4d2ea8a4352d35c9
ShibeDev <echaruhi@gmail.com>
2022-08-14 17:27:55 -0700

    complete function to print repeated strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(input,repeat) {
+    if (repeat < 0) return 'ERROR';
+    let output = ''
+    for(let i =0; i<repeat; i++)
+    {
+        output += input;
+    }
+    return output;
 };
 
 // Do not edit below this line
