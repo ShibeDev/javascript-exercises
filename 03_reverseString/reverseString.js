cb8c5c84a50bef5e327f2f96ea16d8438d27f200
ShibeDev <echaruhi@gmail.com>
2022-08-14 22:31:20 -0700

    reverse a string by adding each char in reversed order

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    if (typeof input !== 'string') return 'ERROR'; 
+    let output = '';
+    let i = input.length;
+    while(i>0){
+        output += input[i-1];
+        i--;
+    }
+    return output;
 };
 
 // Do not edit below this line
